a5066e4895f375ff0045514fbb186d863f213a87
vishal68a <141167171+vishal68a@users.noreply.github.com>
2025-02-17 13:31:19 +0530

    Update 16-bit-cla.v

--- 16-bit-cla.v
+++ 16-bit-cla.v
@@ -1,50 +1,52 @@
+//# 16_bit_cla
 `timescale 1ns / 1ps
-
-////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer:
-//
-// Create Date:   23:23:15 06/13/2023
-// Design Name:   cla_2
-// Module Name:   /home/ise/Desktop/New Folder/cla_2/testbenchcla_2.v
-// Project Name:  cla_2
-// Target Device:  
-// Tool versions:  
+// Engineer: 
+// 
+// Create Date:    21:08:58 06/13/2023 
+// Design Name: 
+// Module Name:    cla_2 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
 // Description: 
 //
-// Verilog Test Fixture created by ISE for module: cla_2
+// Dependencies: 
 //
-// Dependencies:
-// 
-// Revision:
+// Revision: 
 // Revision 0.01 - File Created
-// Additional Comments:
-// 
-////////////////////////////////////////////////////////////////////////////////
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module cla_2(a,b,cin,sum,cout);
+input [15:0] a,b;
+input cin;
+output [15:0] sum;
+output cout;
+wire c1,c2,c3;
 
-module testbenchcla_2;
+cla4bit cla1(.a(a[3:0]),.b(b[3:0]),.cin(cin),.sum(sum[3:0]),.cout(c1));
+cla4bit cla2(.a(a[7:4]),.b(b[7:4]),.cin(c1),.sum(sum[7:4]),.cout(c2));
+cla4bit cla3(.a(a[11:8]),.b(b[11:8]),.cin(c2),.sum(sum[11:8]),.cout(c3));
+cla4bit cla4(.a(a[15:12]),.b(b[15:12]),.cin(c3),.sum(sum[15:12]),.cout(cout));
+endmodule
 
-	
-	reg [15:0] a,b;
-	reg cin;
+module cla4bit(a,b,cin,sum,cout);
+input [3:0] a,b;
+input cin;
+output [3:0] sum;
+output cout;
+wire [3:0] p,g,c;
 
-	
-	wire [15:0] sum;
-	wire cout;
+assign p=a^b;
+assign g=a&b;
 
-	cla_2 uut(.a(a), .b(b), .cin(cin), .sum(sum), .cout(cout));
+assign c[0]=cin;
+assign c[1]=g[0]|(p[0]&c[0]);
+assign c[2]=g[1]|(p[1]&g[0])|(p[2]&p[0]&c[0]);
+assign c[3]=g[2]|(p[2]&g[1])|(p[2]&p[1]&g[0])|(p[2]&p[1]&p[0]&c[0]);
+assign cout=g[3]|(p[3]&g[2])|(p[3]&p[2]&g[1])|(p[3]&p[2]&p[1]&g[0])|(p[3]&p[2]&p[1]&p[0]&c[0]);
+assign sum=p^c;
 
-	initial begin
-		
-		a = 0; b = 0; cin = 0;
-
-		#10 a=16'd0; b=16'd0; cin=1'd1;
-      #10 a=16'd14; b=16'd1; cin=1'd1;
-      #10 a=16'd5; b=16'd0; cin=1'd0;
-      #10 a=16'd999; b=16'd0; cin=1'd1;		
-
-	end
-      initial 
-		$monitor ("a=%d,b=%d,cin=%d,sum=%d,cout=%d", a,b,cin,sum,cout);
-		
 endmodule
